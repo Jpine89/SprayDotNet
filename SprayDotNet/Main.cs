7e258c871f996e6b924c7027c10de7ebf9fc7304
FubarP <jwmpine@gmail.com>
2023-06-02 15:12:40 -0500

    Add project files.

---
+++ SprayDotNet/Main.cs
@@ -0,0 +1,14 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using CitizenFX.Core.NaturalMotion;
+using static CitizenFX.Core.Native.API;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Client
+{
+    internal class Main : BaseScript
+    {
+    }
+}
