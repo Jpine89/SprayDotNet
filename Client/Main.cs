dd4bec84a3cad91f134d0cf559e718b5598990b8
FubarP <jwmpine@gmail.com>
2023-06-08 16:40:40 -0500

    Code Cleanup

--- Client/Main.cs
+++ Client/Main.cs
@@ -10,7 +10,8 @@ namespace Client
 {
     class Main : BaseScript
     {
-        private bool _disposed = false;
+
+#region Variables
         public static Entity HitEntity { get; set; }
         public Vector3 FinalRotation { get; set; }
         private List<Spray> SPRAYS = new List<Spray>();
@@ -21,8 +22,11 @@ namespace Client
         private const int SCAFLEFORM_MIN = 1;
         private const int SCAFLEFORM_MAX = 12;
         private Dictionary<int, int> ScaleFormList = new Dictionary<int, int>();
-
         int rotCam;
+
+#endregion
+
+
         public Main()
         {
             EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
@@ -49,24 +53,11 @@ namespace Client
                 //    args = new[] { "[CarSpawner]", $"I wish I could spawn this {(args.Count > 0 ? $"{args[0]} or" : "")} adder but my owner was too lazy. :(" }
                 //});
             }), false);
-
-            RegisterCommand("dui", new Action(CreateDui), false);
             RegisterCommand("remove", new Action(RemoveSpray), false);
             RegisterCommand("test", new Action<string>(TestCommand), false);
-            RegisterCommand("exit", new Action(EndTest), false);
             RegisterCommand("Decal", new Action(DecalCommand), false);
         }
 
-        private void SaveSpray()
-        {
-            if (isSpray)
-            {
-                isSpray = false;
-                SPRAYS.Add(newSpray);
-                newSpray = new Spray();
-            }
-
-        }
         private async Task LoadScaleForms()
         {
             for (int i = SCAFLEFORM_MIN; i <= SCAFLEFORM_MAX; i++)
@@ -75,27 +66,18 @@ namespace Client
                 if (i < 10)
                     EndValue = "0" + EndValue;
 
-                //Debug.WriteLine("I'm Outside");
-                //Check if Scaleform is loaded
-                //Debug.WriteLine("Outside Key:" + i);
                 if (HasScaleformMovieLoaded(RequestScaleformMovieInteractive("PLAYER_NAME_" + EndValue)))
                 { //If Not Loaded, Check if List Key is open.
-                    //Debug.WriteLine("Key:" + i);
                     if (!ScaleFormList.ContainsKey(i))
                     {// If Open, insert
-                        //Debug.WriteLine("I'm Open");
                         ScaleFormList.Add(i, RequestScaleformMovieInteractive("PLAYER_NAME_" + EndValue));
                     }
                     else
                     {//If key is used, just replace
-                        //Debug.WriteLine("I'm Closed");
                         ScaleFormList[i] = RequestScaleformMovieInteractive("PLAYER_NAME_" + EndValue);
                     } 
                 }
             }
-
-            //Debug.WriteLine(ScaleFormList.Count.ToString());
-            //Debug.WriteLine(ScaleFormList.Count.ToString());
             await Delay(10000);
         }
 
@@ -135,21 +117,95 @@ namespace Client
             }
         }
 
-        private void CreateSpray()
+
+
+
+
+
+
+
+
+
+
+
+        #region Prime Functions
+        private void DrawSpray(int scaleFormHandle, Spray spray)
+            {
+                string SprayUserData = $"<FONT color='{spray.Color}' FACE='Beat Street'> {spray.Text} ";
+
+                //PushScaleformMovieFunction(scaleFormHandle, "SHOW_SHARD_WASTED_MP_MESSAGE");
+                PushScaleformMovieFunction(scaleFormHandle, "SET_PLAYER_NAME");
+                PushScaleformMovieFunctionParameterString(SprayUserData);
+                //PushScaleformMovieFunctionParameterString("Small Text");
+                PushScaleformMovieFunctionParameterInt(5);
+                PopScaleformMovieFunctionVoid();
+
+                //await Delay(0);
+
+                DrawScaleformMovie_3dSolid
+                    (
+                        scaleFormHandle,
+                        spray.LocationCoords.X, spray.LocationCoords.Y, spray.LocationCoords.Z,
+                        spray.RotationCoords.X, spray.RotationCoords.Y, spray.RotationCoords.Z,
+                        (float)1.0, (float)1.0, (float)1.0, //unk values
+                        (float)2.0, (float)2.0, (float)1.0, //Scale X/Y/Z
+                        2 //always 2?
+                    );
+            }
+
+        /*
+         * ToDo
+         * Add not only Functionality, but new class to handle this
+         */
+        private void RemoveSpray()
+            {
+                //Has Info 
+                //p_loose_rag_01_s  -> 921993182
+                var ragProp = CreateObject(
+                                921993182,
+                                (float)0.0, (float)0.0, (float)0.0,
+                                true, false, false
+                                );
+
+                var ped = PlayerPedId();
+                var test = GetPedBoneIndex(ped, 28422);
+
+                Debug.WriteLine(ped.ToString());
+                Debug.WriteLine(test.ToString());
+
+                AttachEntityToEntity(ragProp, ped, GetPedBoneIndex(ped, 28422), (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, true, true, false, true, 1, true);
+            }
+
+        /*
+         * Logic will want to be built out more on this when
+         * Database Logic is being built. Plus NUI
+         */
+        private void SaveSpray()
         {
+            if (isSpray)
+            {
+                isSpray = false;
+                SPRAYS.Add(newSpray);
+                newSpray = new Spray();
+            }
 
         }
 
+        #endregion
+
+        #region Helper Methods
+            //This section of code is just critical
+
         private static void RayCastGamePlayCamera(ref Vector3 endPoint, ref Vector3 rotation)
         {
-            
+
             float distance = 10f;
             var cameraRotation = GetGameplayCamRot(0); //if ped not working try 0
             var cameraCoord = GetGameplayCamCoord();
             var dir = RotationToDirection(cameraRotation);
 
             Vector3 Des = new Vector3()
-            {   
+            {
                 X = cameraCoord.X + dir.X * distance,
                 Y = cameraCoord.Y + dir.Y * distance,
                 Z = cameraCoord.Z + dir.Z * distance
@@ -200,7 +256,7 @@ namespace Client
             {
                 await Delay(0);
 
-                if(WantedLocation != null && WantedRotation != null)
+                if (WantedLocation != null && WantedRotation != null)
                 {
                     if (reCheck >= 0)
                         reCheck--;
@@ -232,11 +288,16 @@ namespace Client
             }
         }
 
-        private void CreateDui()
-        {
+        #endregion
 
-        }
+        #region Obselete Functions
 
+        /*
+         * This should be kept for looking into later
+         * It's possible that this command may be lighter
+         * then the ScaleForm, plus you can use this
+         * in relation of vehicles. 
+        */
         private async void DecalCommand()
         {
             Vector3 spradyData = new Vector3(16, 25, 73);
@@ -250,7 +311,7 @@ namespace Client
             //float opacity, float timeout, bool p17, bool p18, bool p19
 
             RequestStreamedTextureDict("commonmenu", false);
-            int i  = AddDecal(1110, 16, 25, 73,
+            int i = AddDecal(1110, 16, 25, 73,
                 1f, 1f, 1f,//dir X/Y/Z
                 0, 0, 0,
                 2f, 4f, //width height
@@ -285,124 +346,74 @@ namespace Client
             //}
         }
 
-        private void RemoveSpray()
-        {
-            //Has Info 
-            //p_loose_rag_01_s  -> 921993182
-            var ragProp = CreateObject(
-                            921993182,
-                            (float)0.0, (float)0.0, (float)0.0,
-                            true, false, false
-                            );
 
-            var ped = PlayerPedId();
-            var test = GetPedBoneIndex(ped, 28422);
-
-            Debug.WriteLine(ped.ToString());
-            Debug.WriteLine(test.ToString());
-
-            AttachEntityToEntity(ragProp, ped, GetPedBoneIndex(ped, 28422), (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, true, true, false, true, 1, true);
-        }
-
+        /*
+         * Set for Deletion later.
+         * Was a reference for underestanding concept of OnClientResourceStart Functions.
+        */
         private void OnClientResourceStart(string resourceName)
-        {
-            if (GetCurrentResourceName() != resourceName) return;
-
-            RegisterCommand("car", new Action<int, List<object>, string>((source, args, raw) =>
             {
-                // TODO: make a vehicle! fun!
-                TriggerEvent("chat:addMessage", new
+                if (GetCurrentResourceName() != resourceName) return;
+
+                RegisterCommand("car", new Action<int, List<object>, string>((source, args, raw) =>
                 {
-                    color = new[] { 255, 0, 0 },
-                    args = new[] { "[CarSpawner]", $"I wish I could spawn this {(args.Count > 0 ? $"{args[0]} or" : "")} adder but my owner was too lazy. :(" }
-                });
-            }), false);
-        }
-
-        private async void DrawSpray(int scaleFormHandle, Spray spray)
-        {
-            string SprayUserData = $"<FONT color='{spray.Color}' FACE='Beat Street'> {spray.Text} ";
-
-            //Debug.WriteLine("ScaleForm: " + scaleFormHandle);
-            //Debug.WriteLine("HasFormLoaded: " + HasScaleformMovieLoaded(scaleFormHandle));
-
-            //rewrite this into it's own tick, may help with performance
-            //plus it's a while loop inside a tic..
-            //var scaleForm = RequestScaleformMovie("mp_big_message_freemode");
-            //while (!HasScaleformMovieLoaded(scaleForm))
-            //{
-            //    await Delay(0);
-            //}
-
-            //PushScaleformMovieFunction(scaleFormHandle, "SHOW_SHARD_WASTED_MP_MESSAGE");
-            PushScaleformMovieFunction(scaleFormHandle, "SET_PLAYER_NAME");
-            PushScaleformMovieFunctionParameterString(SprayUserData);
-            PushScaleformMovieFunctionParameterString("Small Text");
-            PushScaleformMovieFunctionParameterInt(5);
-            PopScaleformMovieFunctionVoid();
-
-            //await Delay(0);
-
-            DrawScaleformMovie_3dSolid
-                (
-                    scaleFormHandle,
-                    spray.LocationCoords.X, spray.LocationCoords.Y, spray.LocationCoords.Z, //16f, 25f, 73f,
-                    spray.RotationCoords.X, spray.RotationCoords.Y, spray.RotationCoords.Z,
-                    //0,0,0,
-                    (float)1.0, (float)1.0, (float)1.0, //unk values
-                    (float)2.0, (float)2.0, (float)1.0, //Scale X/Y/Z
-                    2 //always 2?
-                );
-        }
-
-        private async void TestCommand(string testString)
-        {
-            _disposed = true;
-            var test = GetGameplayCamCoords();
-            var test2 = GetGameplayCamFov();
-            var test3 = GetGameplayCamRot(0);
-            //var alpha = 10 * math.floor(scale * 40);
-            Debug.WriteLine(test.ToString());
-
-            //var teest4 = CreateRuntimeTxd("test");
-
-            RequestStreamedTextureDict("commonmenu", true);
-
-            //var t = FindRaycastedSprayCoords();
-            Vector3 spradyData = new Vector3(16, 25, 73);
-
-            //PrivateFontCollection collection = new PrivateFontCollection();
-            //collection.AddFontFile(@"C:\Projects\MyProj\free3of9.ttf");
-            //FontFamily fontFamily = new FontFamily("Free 3 of 9", collection);
-            //Font font = new Font(fontFamily, height);
-
-
-            //RegisterFontFile();
-
-            while (_disposed)
-            {
-                await Delay(0);
-                SetTextColour(255, 0, 0, 0);
-
-                //BeginTextCommandWidth("THREESTRINGS");
-                //EndTextCommandGetWidth(1);
-
-                //BeginTextCommandDisplayText("THREESTRINGS");
-                //AddTextComponentString("TEST");
-                //EndTextCommandDisplayText((float)0.50, (float)0.50);
-                //EndTextCommandDisplayHelp();
-                DrawSprite("commonmenu", "shop_box_cross", (float)0.10, (float)0.10, (float)0.50, (float)0.50, 0, 255, 255, 255, 150);
-                //SetTextEntry("STRING");
-                //AddTextComponentString("This is a test");
-                //DrawText((float)0.50, (float)0.50);
+                    // TODO: make a vehicle! fun!
+                    TriggerEvent("chat:addMessage", new
+                    {
+                        color = new[] { 255, 0, 0 },
+                        args = new[] { "[CarSpawner]", $"I wish I could spawn this {(args.Count > 0 ? $"{args[0]} or" : "")} adder but my owner was too lazy. :(" }
+                    });
+                }), false);
             }
 
-            //DrawSprite("dpscenes", "Circle", test.X, test.Y, 100, 100, 100, 255, 0, 0, 40);
-        }
+        /*
+            * Obselete code from DPScene. Keeping for reference later.
+            * Potential use for a "Cheap" Spray concept that wouldn't require
+            * any heavy math or a scaleform?
+        */
+        private async void TestCommand(string testString)
+            {
+                var test = GetGameplayCamCoords();
+                var test2 = GetGameplayCamFov();
+                var test3 = GetGameplayCamRot(0);
+                //var alpha = 10 * math.floor(scale * 40);
+                Debug.WriteLine(test.ToString());
 
-        private void EndTest()
-        {
-            _disposed = false;
-        }
+                //var teest4 = CreateRuntimeTxd("test");
+
+                RequestStreamedTextureDict("commonmenu", true);
+
+                //var t = FindRaycastedSprayCoords();
+                Vector3 spradyData = new Vector3(16, 25, 73);
+
+                //PrivateFontCollection collection = new PrivateFontCollection();
+                //collection.AddFontFile(@"C:\Projects\MyProj\free3of9.ttf");
+                //FontFamily fontFamily = new FontFamily("Free 3 of 9", collection);
+                //Font font = new Font(fontFamily, height);
+
+
+                //RegisterFontFile();
+
+                while (true)
+                {
+                    await Delay(0);
+                    SetTextColour(255, 0, 0, 0);
+
+                    //BeginTextCommandWidth("THREESTRINGS");
+                    //EndTextCommandGetWidth(1);
+
+                    //BeginTextCommandDisplayText("THREESTRINGS");
+                    //AddTextComponentString("TEST");
+                    //EndTextCommandDisplayText((float)0.50, (float)0.50);
+                    //EndTextCommandDisplayHelp();
+                    DrawSprite("commonmenu", "shop_box_cross", (float)0.10, (float)0.10, (float)0.50, (float)0.50, 0, 255, 255, 255, 150);
+                    //SetTextEntry("STRING");
+                    //AddTextComponentString("This is a test");
+                    //DrawText((float)0.50, (float)0.50);
+                }
+
+                //DrawSprite("dpscenes", "Circle", test.X, test.Y, 100, 100, 100, 255, 0, 0, 40);
+            }
+        #endregion
     }
 }
