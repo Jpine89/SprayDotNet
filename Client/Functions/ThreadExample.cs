9fe78ffe39840d547eb13949762a7548a16cff4b
FubarP <jwmpine@gmail.com>
2023-06-11 15:05:37 -0500

    Added TMC Export

--- Client/Functions/ThreadExample.cs
+++ Client/Functions/ThreadExample.cs
@@ -11,14 +11,23 @@ namespace Client.Functions
     internal class ThreadExample : BaseScript
     {
         bool isOn = false;
+        private dynamic TMC;
+
         public ThreadExample()
         {
+            TMC = Exports["core"].getCoreObject();
             //RegisterScript(this);
-            UnregisterScript(this);
+            //UnregisterScript(this);
             //Interval 1000;
             //Tick += ThreadExample_Tick;
 
-            RegisterCommand("thread", new Action(TestThreads), false);
+            //RegisterCommand("thread", new Action(TestThreads), false);
+        }
+
+        [Command("DoIt")]
+        private void test()
+        {
+            Debug.WriteLine("Do it");
         }
 
         private async Task ThreadExample_Tick()
