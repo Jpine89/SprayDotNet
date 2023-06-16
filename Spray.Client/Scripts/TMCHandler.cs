eafd32e5cba60b42fd3d5745799685aa560fae64
FubarP <jwmpine@gmail.com>
2023-06-16 15:06:04 -0500

    Removing ThreadExample Class in place for TMCHanlder

--- Spray.Client/Scripts/TMCHandler.cs
+++ Spray.Client/Scripts/TMCHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,7 +39,86 @@ namespace Spray.Client.Scripts
         private void TestEvent(int source, List<object> arguments, string raw)
         {
             Debug.WriteLine("Test Event Called");
-            BaseScript.TriggerEvent("pspray:test");
+            Elements elm = new Elements()
+            {
+                type = "text",
+                name = "text",
+                label = "Text",
+                value = "tester",
+                multiline = true
+            };
+            Elements confirmbtn = new Elements()
+            {
+                type = "button",
+                name = "button_test",
+                label = "Button Test",
+                icon = "fad fa-heartbeat",
+                disabled = false
+            };
+            Elements[] elements = { elm, confirmbtn };
+            Dictionary<string, object> setting = new() {
+                {"namespace", "testing_namespace" },
+                {"test", "openMenu" },
+                {"title", "Edit text" },
+                {"subtitle", "If you have a speciifc" },
+                {"form", true }
+            };
+            _tmc.Functions.OpenMenu(setting, elements, new Action<dynamic, bool>(close), new Action(something), new Action<dynamic>(testFunc));
+            //BaseScript.TriggerEvent("pspray:test");
         }
+        private void close(dynamic change, bool confirm)
+        {
+            TmcChangeFunc sendToSave = new TmcChangeFunc();
+            sendToSave.SaveSpray = false;
+            if (confirm)
+            {
+                string test = JsonConvert.SerializeObject(change);
+                sendToSave = JsonConvert.DeserializeObject<TmcChangeFunc>(test);
+                Debug.WriteLine("test close");
+                Debug.WriteLine(test);
+                Debug.WriteLine($"{confirm}");
+                sendToSave.SaveSpray = true;
+            }
+            //Spray_Function.SaveSpray(sendToSave);
+            BaseScript.TriggerEvent("pspray:SaveSpray", sendToSave.Text, sendToSave.SaveSpray);
+        }
+        private void something()
+        {
+            Debug.WriteLine("test Something");
+        }
+        private void testFunc(dynamic change)
+        {
+            Debug.WriteLine("test func");
+            string test = JsonConvert.SerializeObject(change);
+
+
+            Debug.WriteLine(test);
+            var changed = JsonConvert.DeserializeObject<TmcChangeFunc>(test);
+            Debug.WriteLine(changed.NewValue);
+            BaseScript.TriggerEvent("pspray:spray_text_update", changed.NewValue);
+        }
+    }
+    public class Elements
+    {
+        public string type { get; set; }
+        public string name { get; set; }
+        public string label { get; set; }
+        public string value { get; set; }
+        public bool multiline { get; set; }
+        public bool form { get; set; }
+        public string cat { get; set; }
+        public string icon { get; set; }
+        public bool disabled { get; set; }
+
+    }
+
+    public class TmcChangeFunc
+    {
+        public string NewValue { get; set; }
+        public string ElementChanged { get; set; }
+        public string OldValue { get; set; }
+        public string MenuNamespace { get; set; }
+        public string Text { get; set; }
+        public bool SaveSpray { get; set; }
     }
 }
