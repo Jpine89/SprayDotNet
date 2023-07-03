adca645d1a03053c1862bfcdedeef480cd41a9fc
FubarP <jwmpine@gmail.com>
2023-07-03 10:32:59 -0500

    Fixed compiling issues, and got corrected libraries.

---
+++ PSpray.Server/Database.cs
@@ -0,0 +1,26 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSpray.Server
+{
+    class Database
+    {
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(GetConnectionString());
+        }
+
+        private static string GetConnectionString()
+        {
+            string stringReturn = API.GetConvar("mysql_connection_string", null);
+            if (stringReturn == null) Debug.WriteLine("Convars in Config are not set!");
+
+            string actualString = $"Server=localhost; Port=3306; User ID=root; Password=pineapple; Database=QBCoreFramework_A237FE;";
+            return actualString;
+        }
+    }
+}
--- PSpray.Server/Main.cs
+++ PSpray.Server/Main.cs
@@ -1,7 +1,5 @@
 ﻿using CitizenFX.Core;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace PSpray.Server
 {
@@ -9,7 +7,30 @@ namespace PSpray.Server
     {
         public Main()
         {
+            EventHandlers["init_player"] += new Action<Player>(InitPlayer);
+
             Debug.WriteLine("Server Init");
+
+            initConnect();
+        }
+
+        private void initConnect()
+        {
+            using (var connection = Database.GetConnection())
+            {
+                Debug.WriteLine("Connection?");
+            }
+        }
+
+        private async void InitPlayer([FromSource] Player source)
+        {
+            Debug.WriteLine("Calling Init Player!");
+            //var parameters = new { Identifier = source.Identifiers["license"], Money = 5000, XP = 0 };
+            //using (var connection = Database.GetConnection())
+            //{
+            //    int rowsAffected = await connection.ExecuteAsync(Queries.initPlayer, parameters);
+            //    Debug.WriteLine($"Player Init rowsAffected: {rowsAffected}");
+            //}
         }
     }
 }
