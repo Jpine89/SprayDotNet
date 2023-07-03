2c79b0e8460071d8086b04e44dba0bc5ab103597
FubarP <jwmpine@gmail.com>
2023-07-02 22:39:04 -0500

    Created Main class for Server

---
+++ PSpray.Server/Main.cs
@@ -0,0 +1,15 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSpray.Server
+{
+    class Main : BaseScript
+    {
+        public Main()
+        {
+            Debug.WriteLine("Server Init");
+        }
+    }
+}
