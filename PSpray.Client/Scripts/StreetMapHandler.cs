aea5aa6af8fe7b8dc1487177173d6258319b7cef
FubarP <jwmpine@gmail.com>
2023-08-12 19:23:15 -0500

    Street POC initial Design

---
+++ PSpray.Client/Scripts/StreetMapHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSpray.Client.Scripts
+{
+    internal class StreetMapHandler
+    {
+        private static readonly object _padlock = new();
+        private static StreetMapHandler _instance;
+        private StreetMapHandler()
+        {
+            Init();
+            Debug.WriteLine("^2PSpray Tag Handler has been initialised.");
+        }
+
+        internal static StreetMapHandler Instance
+        {
+            get
+            {
+                lock (_padlock)
+                {
+                    return _instance ??= new StreetMapHandler();
+                }
+            }
+        }
+
+        private async void Init()
+        {
+        }
+    }
+}
